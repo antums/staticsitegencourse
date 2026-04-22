661435d606c8f8ece46292ca88714ca8ca9bd28c
antums <aleksantums@gmail.com>
2026-04-22 22:32:43 +0300

    feat: add markdown link and image extraction and tests

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+import re
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_list = []
@@ -19,3 +20,11 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
 
 
     return new_list
+
+def extract_markdown_images(text):
+    matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
+
+def extract_markdown_links(text):
+    matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
