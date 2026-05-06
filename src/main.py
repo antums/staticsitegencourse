03918eb6a68a365a637e45de46d3fea4670a62ac
antums <aleksantums@gmail.com>
2026-05-06 18:43:26 +0300

    feat: add page generation from Markdown templates

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 import os
 import shutil
 from copystatic import copy_files_recursive
+from gencontent import generate_page
 
 dir_static = "./static"
 dir_public = "./public"
+dir_content = "./content"
+dir_template = "./template.html"
 
 def main():
     print("Deleting public directory...")
@@ -13,6 +16,11 @@ def main():
     print("Copying static files to public directory...")
     copy_files_recursive(dir_static, dir_public)
 
-
+    print("Generating page...")
+    generate_page(
+        os.path.join(dir_content, "index.md"),
+        dir_template,
+        os.path.join(dir_public, "index.html"),
+    )
 if __name__ == "__main__":
     main()
