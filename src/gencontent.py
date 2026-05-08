9fb5163734952b53dc06d9e31ea2a2d1a17f8f92
antums <aleksantums@gmail.com>
2026-05-08 20:59:31 +0300

    feat: configure site for GitHub pages deployment

--- src/gencontent.py
+++ src/gencontent.py
@@ -2,18 +2,18 @@ import os
 from pathlib import Path
 from markdown_blocks import markdown_to_html_node
 
-def generate_page_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_page_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for item in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, item)
         to_path = os.path.join(dest_dir_path, item)
         if os.path.isfile(from_path):
             to_path = Path(to_path).with_suffix(".html")
-            generate_page(from_path, template_path, to_path)
+            generate_page(from_path, template_path, to_path, basepath)
         else:
-            generate_page_recursive(from_path, template_path, to_path)
+            generate_page_recursive(from_path, template_path, to_path, basepath)
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     from_file = open(from_path, "r")
     markdown = from_file.read()
@@ -29,6 +29,8 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
+    template = template.replace('href="/', f'href="{basepath}')
+    template = template.replace('src="/', f'src="{basepath}')
 
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path:
--- src/main.py
+++ src/main.py
@@ -1,14 +1,22 @@
 import os
 import shutil
+import sys
 from copystatic import copy_files_recursive
 from gencontent import generate_page_recursive
 
+
 dir_static = "./static"
-dir_public = "./public"
+dir_public = "./docs"
 dir_content = "./content"
 dir_template = "./template.html"
+default_basepath = "/"
+
 
 def main():
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+        
     print("Deleting public directory...")
     if os.path.exists(dir_public):
         shutil.rmtree(dir_public)
@@ -17,6 +25,6 @@ def main():
     copy_files_recursive(dir_static, dir_public)
 
     print("Generating page...")
-    generate_page_recursive(dir_content, dir_template, dir_public)
+    generate_page_recursive(dir_content, dir_template, dir_public, basepath)
 if __name__ == "__main__":
     main()
